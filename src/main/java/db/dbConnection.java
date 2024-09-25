2d307fdc4c06acb55a99b0dcae0e1921ef8d641e
chiragmishra00 <chirag.m@aptcoder.in>
2024-09-25 22:39:12 +0530

    completed

--- src/main/java/db/dbConnection.java
+++ src/main/java/db/dbConnection.java
@@ -1,33 +1,33 @@
 package db;
+
 import java.sql.*;
+
 public class dbConnection {
     public static Connection connection;
     private static final String url = "jdbc:mysql://127.0.0.1:3306/fileHider";
-    private static final String username = "root";
-    private static final String pass = "chirag";
-    public static Connection getConnection(){
-        try{
+    private static final String username = "";
+    private static final String pass = "";
+
+    public static Connection getConnection() {
+        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(url,username,pass);
-        }
-        catch(Exception e){
+            connection = DriverManager.getConnection(url, username, pass);
+        } catch (Exception e) {
             e.printStackTrace();
             System.out.println(e);
         }
         return connection;
     }
 
-
-    public static void close(){
+    public static void close() {
         try {
-            if(connection!=null) {
+            if (connection != null) {
                 connection.close();
             }
         } catch (SQLException e) {
-           e.printStackTrace();
-           System.out.println(e);
+            e.printStackTrace();
+            System.out.println(e);
         }
     }
-
 
 }
